1173ead866e67d2e32679c7fe3fae620679e035d
J U <jairo20055@hotmail.com>
2022-07-23 18:15:25 -0500

    Ejercicios practicas

---
+++ InicioPrimerEjercicio/src/inicioprimerejercicio/InicioPrimerEjercicio.java
@@ -0,0 +1,26 @@
+
+package inicioprimerejercicio;
+
+
+public class InicioPrimerEjercicio {
+
+    
+    public static void main(String[] args) {
+        int num1 = 10;
+        long num2 = 8;
+        double num3 = 15.5;
+        boolean condicion=true;
+        String saludo = "Hola chicos!!";
+        char letra = 'B';
+        
+        System.out.println("El numero entero es: "+num1);
+        System.out.println("El numero long es: "+num2);
+        System.out.println("El numero double es: "+num3);
+        System.out.println("La condicion actual es: "+condicion);
+        System.out.println("Saluda por favor,  "+saludo);
+        System.out.println("La letra es: "+letra);
+               
+        
+    }
+    
+}
